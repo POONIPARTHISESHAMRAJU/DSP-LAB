e25842bcbf9a288d510449d4597dac105907a344
POONIPARTHISESHAMRAJU <58951557+POONIPARTHISESHAMRAJU@users.noreply.github.com>
2019-12-18 22:07:50 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+#code to append one array into  another arrary
+import numpy as l
+a=l.array(input('enter an array')
+b=input('enter an an array')
+for i in range(0,len(b)):
+	a.append(b[i])
+	i=i+1
+print a
